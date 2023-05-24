55b5a28ec3aef539ca91279ad0b537dfebb7f8c2
vannhan2k <nguyenvannhan112200@gmail.com>
2023-05-24 21:40:45 +0700

    CHANGE FOOTER

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,9 +2,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy;Online Library Management System |<a href="https://itsourcecode.com/" target="_blank"  > Designed by : ITSOURCECODE</a> 
+                   &copy;Web app HỆ THÔNG QUẢN LÝ THƯ VIỆN
                 </div>
 
             </div>
         </div>
     </section>
+    CÁI TREN LÀ TRONG TRƯỜNG HỢP CÓ NHIỀU FILE SỬA THÌ BẤM STAGE CHANGE ALL CÒN CÁI DƯỚI THÌ 1 FILE
