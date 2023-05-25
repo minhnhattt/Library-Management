fcefdedda938c6a45a8743274623f03e53cfc0f9
minhnhattt <n.minhnhat2304@gmail.com>
2023-05-25 14:10:34 +0700

    fix manage-authors

--- admin/manage-authors.php
+++ admin/manage-authors.php
@@ -2,22 +2,25 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-
-if (strlen($_SESSION['alogin']) == 0) {
-    header('location:index.php');
-} else {
-    if (isset($_GET['del'])) {
-        $id = $_GET['del'];
-        $sql = "DELETE FROM tblauthors WHERE id=:id";
-        $query = $dbh->prepare($sql);
-        $query->bindParam(':id', $id, PDO::PARAM_STR);
-        $query->execute();
-        $_SESSION['delmsg'] = "Author deleted";
-        header('location:manage-authors.php');
-    }
+if(strlen($_SESSION['alogin'])==0)
+    {   
+header('location:index.php');
 }
-?>
+else{
+if(isset($_GET['del']))
+{
+$id=$_GET['del'];
+$sql = "delete from tblauthors  WHERE id=:id";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':id',$id, PDO::PARAM_STR);
+$query -> execute();
+$_SESSION['delmsg']="Author deleted";
+header('location:manage-authors.php');
 
+}
+
+
+    ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -36,114 +39,125 @@ if (strlen($_SESSION['alogin']) == 0) {
     <link href="assets/css/style.css" rel="stylesheet" />
     <!-- GOOGLE FONT -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
 </head>
 <body>
-    <!------MENU SECTION START-->
-    <?php include('includes/header.php'); ?>
-    <!-- MENU SECTION END-->
+      <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->
     <div class="content-wrapper">
-        <div class="container">
-            <div class="row pad-botm">
-                <div class="col-md-12">
-                    <h4 class="header-line">Quản lý Tác giả</h4>
-                </div>
-            </div>
-            <div class="row">
-                <?php if ($_SESSION['error'] != "") { ?>
-                    <div class="col-md-6">
-                        <div class="alert alert-danger">
-                            <strong>Error :</strong>
-                            <?php echo htmlentities($_SESSION['error']); ?>
-                            <?php echo htmlentities($_SESSION['error'] = ""); ?>
-                        </div>
-                    </div>
-                <?php } ?>
-                <?php if ($_SESSION['msg'] != "") { ?>
-                    <div class="col-md-6">
-                        <div class="alert alert-success">
-                            <strong>Success :</strong>
-                            <?php echo htmlentities($_SESSION['msg']); ?>
-                            <?php echo htmlentities($_SESSION['msg'] = ""); ?>
-                        </div>
-                    </div>
-                <?php } ?>
-                <?php if ($_SESSION['updatemsg'] != "") { ?>
-                    <div class="col-md-6">
-                        <div class="alert alert-success">
-                            <strong>Success :</strong>
-                            <?php echo htmlentities($_SESSION['updatemsg']); ?>
-                            <?php echo htmlentities($_SESSION['updatemsg'] = ""); ?>
-                        </div>
-                    </div>
-                <?php } ?>
-                <?php if ($_SESSION['delmsg'] != "") { ?>
-                    <div class="col-md-6">
-                        <div class="alert alert-success">
-                            <strong>Success :</strong>
-                            <?php echo htmlentities($_SESSION['delmsg']); ?>
-                            <?php echo htmlentities($_SESSION['delmsg'] = ""); ?>
-                        </div>
-                    </div>
-                <?php } ?>
-            </div>
-        </div>
-        <div class="row">
+         <div class="container">
+        <div class="row pad-botm">
             <div class="col-md-12">
-                <!-- Advanced Tables -->
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Danh sách tác giả
-                    </div>
-                    <div class="panel-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Tên tác giả</th>
-                                        <th>Ngày thêm</th>
-                                        <th>Ngày sửa gần nhất</th>
-                                        <th>Chức năng</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    $sql = "SELECT * FROM tblauthors";
-                                    $query = $dbh->prepare($sql);
-                                    $query->execute();
-                                    $results = $query->fetchAll(PDO::FETCH_OBJ);
-                                    $cnt = 1;
-                                    if ($query->rowCount() > 0) {
-                                        foreach ($results as $result) {
-                                    ?>
-                                            <tr class="odd gradeX">
-                                                <td class="center"><?php echo htmlentities($cnt); ?></td>
-                                                <td class="center"><?php echo htmlentities($result->AuthorName); ?></td>
-                                                <td class="center"><?php echo htmlentities($result->creationDate); ?></td>
-                                                <td class="center"><?php echo htmlentities($result->UpdationDate); ?></td>
-                                                <td class="center">
-                                                    <a href="edit-author.php?athrid=<?php echo htmlentities($result->id); ?>"><button class="btn btn-primary"><i class="fa fa-edit "></i> Sửa</button></a>
-                                                    <a href="manage-authors.php?del=<?php echo htmlentities($result->id); ?>" onclick="return confirm('Are you sure you want to delete?');"><button class="btn btn-danger"><i class="fa fa-pencil"></i> Xoá</button></a>
-                                                </td>
-                                            </tr>
-                                    <?php
-                                            $cnt = $cnt + 1;
-                                        }
-                                    }
-                                    ?>
-                                </tbody>
-                            </table>
+                <h4 class="header-line">Quản lý Tác giả</h4>
+    </div>
+     <div class="row">
+    <?php if($_SESSION['error']!="")
+    {?>
+<div class="col-md-6">
+<div class="alert alert-danger" >
+ <strong>Error :</strong> 
+ <?php echo htmlentities($_SESSION['error']);?>
+<?php echo htmlentities($_SESSION['error']="");?>
+</div>
+</div>
+<?php } ?>
+<?php if($_SESSION['msg']!="")
+{?>
+<div class="col-md-6">
+<div class="alert alert-success" >
+ <strong>Success :</strong> 
+ <?php echo htmlentities($_SESSION['msg']);?>
+<?php echo htmlentities($_SESSION['msg']="");?>
+</div>
+</div>
+<?php } ?>
+<?php if($_SESSION['updatemsg']!="")
+{?>
+<div class="col-md-6">
+<div class="alert alert-success" >
+ <strong>Success :</strong> 
+ <?php echo htmlentities($_SESSION['updatemsg']);?>
+<?php echo htmlentities($_SESSION['updatemsg']="");?>
+</div>
+</div>
+<?php } ?>
+
+
+   <?php if($_SESSION['delmsg']!="")
+    {?>
+<div class="col-md-6">
+<div class="alert alert-success" >
+ <strong>Success :</strong> 
+ <?php echo htmlentities($_SESSION['delmsg']);?>
+<?php echo htmlentities($_SESSION['delmsg']="");?>
+</div>
+</div>
+<?php } ?>
+
+</div>
+
+
+        </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- Advanced Tables -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Danh sách tác giả
+                        </div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Tên tác giả</th>
+                                            <th>Ngày thêm</th>
+                                            <th>Ngày sửa gần nhất</th>
+                                            <th>Chức năng</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+<?php $sql = "SELECT * from  tblauthors";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{               ?>                                      
+                                        <tr class="odd gradeX">
+                                            <td class="center"><?php echo htmlentities($cnt);?></td>
+                                            <td class="center"><?php echo htmlentities($result->AuthorName);?></td>
+                                            <td class="center"><?php echo htmlentities($result->creationDate);?></td>
+                                            <td class="center"><?php echo htmlentities($result->UpdationDate);?></td>
+                                            <td class="center">
+
+                                            <a href="edit-author.php?athrid=<?php echo htmlentities($result->id);?>"><button class="btn btn-primary"><i class="fa fa-edit "></i> Sửa</button> 
+                                          <a href="manage-authors.php?del=<?php echo htmlentities($result->id);?>" onclick="return confirm('Are you sure you want to delete?');"" >  <button class="btn btn-danger"><i class="fa fa-pencil"></i> Xoá</button>
+                                            </td>
+                                        </tr>
+ <?php $cnt=$cnt+1;}} ?>                                      
+                                    </tbody>
+                                </table>
+                            </div>
+                            
                         </div>
                     </div>
+                    <!--End Advanced Tables -->
                 </div>
-                <!--End Advanced Tables -->
             </div>
-        </div>
+
+
+            
+    </div>
     </div>
 
-    <!-- CONTENT-WRAPPER SECTION END-->
-    <?php include('includes/footer.php'); ?>
-    <!-- FOOTER SECTION END-->
+     <!-- CONTENT-WRAPPER SECTION END-->
+  <?php include('includes/footer.php');?>
+      <!-- FOOTER SECTION END-->
     <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
     <!-- CORE JQUERY  -->
     <script src="assets/js/jquery-1.10.2.js"></script>
@@ -152,7 +166,8 @@ if (strlen($_SESSION['alogin']) == 0) {
     <!-- DATATABLE SCRIPTS  -->
     <script src="assets/js/dataTables/jquery.dataTables.js"></script>
     <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
-    <!-- CUSTOM SCRIPTS  -->
+      <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
 </body>
 </html>
+<?php } ?>
