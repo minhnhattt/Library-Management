3279bd0047260324e6ab35df07da89eb0566c7a4
vannhan2k <nguyenvannhan112200@gmail.com>
2023-05-29 08:55:27 +0700

    fix đăng ký

--- signup.php
+++ signup.php
@@ -1,8 +1,7 @@
-<?php  
+<?php 
 session_start();
 include('includes/config.php');
 error_reporting(0);
-<<<<<<< HEAD
 if(isset($_POST['signup']))
 {
 //code for captach verification
@@ -31,55 +30,20 @@ $query->bindParam(':password',$password,PDO::PARAM_STR);
 $query->bindParam(':status',$status,PDO::PARAM_STR);
 $query->execute();
 $lastInsertId = $dbh->lastInsertId();
-if($lastInsertId)
-{
-echo '<script>alert("Your Registration successfull and your student id is  "+"'.$StudentId.'")</script>';
+if ($lastInsertId) {
+    echo '<script>alert("Your Registration successful and your student id is " + "' . $StudentId . '")</script>';
+    header("Location: index.php"); // Redirect to index.php
+    exit(); // Stop further execution of the script
+} else {
+    echo "<script>alert('Something went wrong. Please try again');</script>";
 }
-else 
-{
-echo "<script>alert('Something went wrong. Please try again');</script>";
+
 }
-}
-=======
-if (isset($_POST['signup'])) {
-    //code for captach verification
-    if (isset($_POST['signup'])) {
-        //Code for student ID
-        $count_my_page = ("studentid.txt");
-        $hits = file($count_my_page);
-        $hits[0]++;
-        $fp = fopen($count_my_page, "w");
-        fputs($fp, "$hits[0]");
-        fclose($fp);
-        $StudentId = $hits[0];
-        $fname = $_POST['fullanme'];
-        $mobileno = $_POST['mobileno'];
-        $email = $_POST['email'];
-        $password = md5($_POST['password']);
-        $status = 1;
-        $sql = "INSERT INTO  tblstudents(StudentId,FullName,MobileNumber,EmailId,Password,Status) VALUES(:StudentId,:fname,:mobileno,:email,:password,:status)";
-        $query = $dbh->prepare($sql);
-        $query->bindParam(':StudentId', $StudentId, PDO::PARAM_STR);
-        $query->bindParam(':fname', $fname, PDO::PARAM_STR);
-        $query->bindParam(':mobileno', $mobileno, PDO::PARAM_STR);
-        $query->bindParam(':email', $email, PDO::PARAM_STR);
-        $query->bindParam(':password', $password, PDO::PARAM_STR);
-        $query->bindParam(':status', $status, PDO::PARAM_STR);
-        $query->execute();
-        $lastInsertId = $dbh->lastInsertId();
-        if ($lastInsertId) {
-            echo '<script>alert("Your Registration successfull and your student id is  "+"' . $StudentId . '")</script>';
-        } else {
-            echo "<script>alert('Something went wrong. Please try again');</script>";
-        }
-    }
->>>>>>> dfe113855a99e26891087b147ffdbbc69fd49490
 }
 ?>
 
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
@@ -88,7 +52,7 @@ if (isset($_POST['signup'])) {
     <!--[if IE]>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <![endif]-->
-    <title>HỆ THỐNG QUẢN LÝ THƯ VIỆN | Đăng ký thành viên</title>
+    <title>Online Library Management System | Student Signup</title>
     <!-- BOOTSTRAP CORE STYLE  -->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <!-- FONT AWESOME STYLE  -->
@@ -97,7 +61,6 @@ if (isset($_POST['signup'])) {
     <link href="assets/css/style.css" rel="stylesheet" />
     <!-- GOOGLE FONT -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-<<<<<<< HEAD
 <script type="text/javascript">
 function valid()
 {
@@ -125,39 +88,10 @@ error:function (){}
 });
 }
 </script>    
-=======
-    <script type="text/javascript">
-        function valid() {
-            if (document.signup.password.value != document.signup.confirmpassword.value) {
-                alert("Password and Confirm Password Field do not match  !!");
-                document.signup.confirmpassword.focus();
-                return false;
-            }
-            return true;
-        }
-    </script>
-    <script>
-        function checkAvailability() {
-            $("#loaderIcon").show();
-            jQuery.ajax({
-                url: "check_availability.php",
-                data: 'emailid=' + $("#emailid").val(),
-                type: "POST",
-                success: function (data) {
-                    $("#user-availability-status").html(data);
-                    $("#loaderIcon").hide();
-                },
-                error: function () { }
-            });
-        }
-    </script>
->>>>>>> dfe113855a99e26891087b147ffdbbc69fd49490
 
 </head>
-
 <body>
     <!------MENU SECTION START-->
-<<<<<<< HEAD
 <?php include('includes/header.php');?>
 <!-- MENU SECTION END-->
     <div class="content-wrapper">
@@ -204,86 +138,22 @@ error:function (){}
 <label>Confirm Password </label>
 <input class="form-control"  type="password" name="confirmpassword" autocomplete="off" required  />
 </div>
-                          
+                            
 <button type="submit" name="signup" class="btn btn-danger" id="submit">Register Now </button>
 
                                     </form>
                             </div>
-=======
-    <?php include('includes/header.php'); ?>
-    <!-- MENU SECTION END-->
-    <div class="content-wrapper">
-        <div class="container">
-            <div class="row pad-botm">
-                <div class="col-md-12">
-                    <h4 class="header-line">User Signup</h4>
-
-                </div>
-
-            </div>
-            <div class="row">
-
-                <div class="col-md-9 col-md-offset-1">
-                    <div class="panel panel-danger">
-                        <div class="panel-heading">
-                            SINGUP FORM
-                        </div>
-                        <div class="panel-body">
-                            <form name="signup" method="post" onSubmit="return valid();">
-                                <div class="form-group">
-                                    <label>Enter Full Name</label>
-                                    <input class="form-control" type="text" name="fullanme" autocomplete="off"
-                                        required />
-                                </div>
-
-
-                                <div class="form-group">
-                                    <label>Mobile Number :</label>
-                                    <input class="form-control" type="text" name="mobileno" maxlength="10"
-                                        autocomplete="off" required />
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Enter Email</label>
-                                    <input class="form-control" type="email" name="email" id="emailid"
-                                        onBlur="checkAvailability()" autocomplete="off" required />
-                                    <span id="user-availability-status" style="font-size:12px;"></span>
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Enter Password</label>
-                                    <input class="form-control" type="password" name="password" autocomplete="off"
-                                        required />
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Confirm Password </label>
-                                    <input class="form-control" type="password" name="confirmpassword"
-                                        autocomplete="off" required />
-                                </div>
-
-                                <button type="submit" name="signup" class="btn btn-danger" id="submit">Register Now
-                                </button>
-
-                            </form>
->>>>>>> dfe113855a99e26891087b147ffdbbc69fd49490
                         </div>
                             </div>
         </div>
     </div>
-<<<<<<< HEAD
     </div>
      <!-- CONTENT-WRAPPER SECTION END-->
     <?php include('includes/footer.php');?>
-=======
-    <!-- CONTENT-WRAPPER SECTION END-->
-    <?php include('includes/footer.php'); ?>
->>>>>>> dfe113855a99e26891087b147ffdbbc69fd49490
     <script src="assets/js/jquery-1.10.2.js"></script>
     <!-- BOOTSTRAP SCRIPTS  -->
     <script src="assets/js/bootstrap.js"></script>
       <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
 </body>
-
 </html>
